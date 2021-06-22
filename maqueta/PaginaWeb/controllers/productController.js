fb1055021dad7b34dc06aab08e0e2cf87c8be496
Nicolas Mizrahi <nicolasmizrahi@MBP-de-Nicolas.fibertel.com.ar>
2021-06-22 10:02:17 -0300

    terminando vistas buscador

--- maqueta/PaginaWeb/controllers/productController.js
+++ maqueta/PaginaWeb/controllers/productController.js
@@ -2,17 +2,17 @@ const db = require('../database/models');
 const op = db.Sequelize.Op;
 const productController = {
     show: function (req, res) { 
-       //let id= req.params.id; //captura el ID del producto
+       let id= req.params.id; //captura el ID del producto
        //este ID lo uso dentro del findByPk para buscar UN solo producto. 
-       //db.Producto.findByPk(id, { 
-         //  include: [
-           //    {association: "usuario" } //agrego la relacion del producto con el usuario
-           //]
-       //})
-       //.then (data => { //toda esta informacion queda almacenada en este "data"
-         return res.render ('product', /*{product: data}, */ { title: 'Proyecto Integrador 2021'}) //renderizo la vista con la info que 
+       db.Producto.findByPk(id, { 
+           include: [
+               {association: "usuario" } //agrego la relacion del producto con el usuario
+           ]
+       })
+       .then (data => { //toda esta informacion queda almacenada en este "data"
+         return res.render ('product', { title: 'Proyecto Integrador 2021', product: data }) //renderizo la vista con la info que 
          //me trajo el findByPk, es decir trajo la info del producto y ahora tamnien la del usuario.
-       //})
+       })
     },
 
     add: function (req, res) { 
@@ -26,7 +26,7 @@ const productController = {
             ]
         })
         .then( relojes =>{
-            return res.render ('index', { title: 'Proyecto Integrador 2021', relojes: relojes})
+            return res.render ('search-results', { title: 'Proyecto Integrador 2021', relojes: relojes})
           // return res.send(data);
         })
         .catch(error =>{
